c2916fc70bc513e796d856cbbdd44976a7add34a
Angel Julian Maldonado Aguilar <julianmaldonadoag@gmail.com>
2021-11-24 13:54:50 -0500

    Load env vars only if the project is running in local

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-// Loading environment variables.
-const path =  require('path')
-let envpath = path.join(__dirname, '..', 'etc', '.env')
-let result = require('dotenv').config({ path: envpath })
-if (result.error) {
-  console.error(".env file not found")
-  console.error(result.error.message)
-  process.exit(1)
+// If the project is running in local server.
+if (process.env.NODE_ENV === 'development') {
+  // Loading environment variables.
+  const path =  require('path')
+  let envpath = path.join(__dirname, '..', 'etc', '.env')
+  let result = require('dotenv').config({ path: envpath })
+  if (result.error) {
+    console.error(".env file not found")
+    console.error(result.error.message)
+    process.exit(1)
+  }
 }
 
 // Including dependencies.
