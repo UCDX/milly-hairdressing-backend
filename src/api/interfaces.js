5646e9605c12617f6a5fbab446baa69357362ae7
Angel Julian Maldonado Aguilar <julianmaldonadoag@gmail.com>
2021-11-26 23:18:08 -0500

    Get all the reservations (blocked time) in specific day

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const userEndpoints = require('./endpoints/users.endpoint')
 const serviceEndpoints = require('./endpoints/services.endpoint')
+const reservationEndpoints = require('./endpoints/reservations.endpoint')
 
 router.use('/users', userEndpoints)
 router.use('/services', serviceEndpoints)
+router.use('/reservations', reservationEndpoints)
 
 module.exports = router
