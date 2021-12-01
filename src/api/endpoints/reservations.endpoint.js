03280a77b1328bd24d297473c584252688758e09
Angel Julian Maldonado Aguilar <julianmaldonadoag@gmail.com>
2021-12-01 15:16:31 -0500

    Fix query params in some endpoints

--- src/api/endpoints/reservations.endpoint.js
+++ src/api/endpoints/reservations.endpoint.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const reservationFlows = require('../flows/reservations.flow');
 
-router.get('/blocked-time', reservationFlows.getBlockedTime)
-router.get('/by-day', reservationFlows.reservationsByDate)
+router.get('/blocked-time/:date', reservationFlows.getBlockedTime)
+router.get('/by-day/:date', reservationFlows.reservationsByDate)
 router.delete('/cancel-reservation', reservationFlows.deleteReservation)
-router.get('/get-specific-reservation', reservationFlows.getSpecificReservation)
+router.get('/get-specific-reservation/:reservation_id', reservationFlows.getSpecificReservation)
 
 module.exports = router
