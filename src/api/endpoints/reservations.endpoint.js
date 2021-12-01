d711faa517bc077376c117a653fca4e54b5d6227
Angel Julian Maldonado Aguilar <julianmaldonadoag@gmail.com>
2021-12-01 08:54:11 -0500

    Retrieve reservations on a certain day

--- src/api/endpoints/reservations.endpoint.js
+++ src/api/endpoints/reservations.endpoint.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const reservationFlows = require('../flows/reservations.flow');
 
 router.get('/blocked-time', reservationFlows.getBlockedTime)
+router.get('/by-day', reservationFlows.reservationsByDate)
 
 module.exports = router
